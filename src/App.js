c747cf3811146c343cfc3c6be9ef2765314529d0
Talha Bin Faisal <talhafaisal2510@gmail.com>
2025-06-06 12:04:39 +0500

    WIP: local changes before switching

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
             <Routes>
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
+              <Route path="/" element={<Register />} />
               
               {/* Protected Admin Routes */}
               <Route element={<ProtectedRoute />}>
